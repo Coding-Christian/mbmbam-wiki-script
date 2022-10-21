afb55390ace5e8c36942c3a132b08f2d968698a4
Christian Haney <christianhaney1996@gmail.com>
2022-10-21 16:20:22 -0700

    v1.2.1

--- config.example.js
+++ config.example.js
@@ -1,3 +1,5 @@
 export default {
-  auth: 'string'
+  auth: 'string',
+  apiURL: 'https://api.spotify.com/v1/shows/308BQUUnIkoH2UAXJCAt0g',
+  castURL: 'https://maximumfun.org/episodes/my-brother-my-brother-and-me/'
 };
--- main.js
+++ main.js
@@ -4,22 +4,18 @@ import fs from 'fs';
 import config from './config.js';
 import tagHandlers from './template/tagHandlers.js';
 const template = fs.readFileSync('./template/content.txt');
-const apiURL = 'https://api.spotify.com/v1/shows/308BQUUnIkoH2UAXJCAt0g';
-const pcURL = 'https://maximumfun.org/episodes/my-brother-my-brother-and-me/';
-const extraEpisodes = 12; //extra clips on Spotify that aren't main episodes
-let apiData = [];
-let webData = '';
-let token = '';
+const episodeNum = Number(process.argv[2]);
 
 //utility functions
 const getEpNum = name => Number(name.split(':')[0].replace(/\D/g, ''));
 
 //script
 function script() {
-  const episodeNum = Number(process.argv[2]);
-
-  if (!config.auth || !episodeNum || !template || !tagHandlers) {
+  if (!episodeNum || !template || !tagHandlers) {
     throw new Error(`Error: bad arguements (${episodeNum})`);
+  }
+  if (!config.auth || !config.apiURL || !config.castURL) {
+    throw new Error(`Error: bad config (${config})`);
   }
 
   axios
@@ -29,56 +25,56 @@ function script() {
       }
     })
     .then(res => {
-      token = res.data.access_token;
+      const token = res.data.access_token;
 
       axios
-        .get(apiURL + '/episodes?market=US&limit=1', {
+        .get(config.apiURL + '/episodes?market=US&limit=1', {
           headers: {
             Authorization: 'Bearer ' + token
           }
         })
         .then(res => {
-          const offset = getEpNum(res.data.items[0].name) - episodeNum - 1;
+          const lastEpNum = getEpNum(res.data.items[0].name);
+          const offset = lastEpNum - episodeNum - 1;
+          const extraEpisodes = res.data.total - lastEpNum;
 
           axios
-            .get(`${apiURL}/episodes?market=US&limit=${extraEpisodes}&offset=${offset > 0 ? offset : 0}`, {
+            .get(`${config.apiURL}/episodes?market=US&limit=${extraEpisodes}&offset=${offset > 0 ? offset : 0}`, {
               headers: {
                 Authorization: 'Bearer ' + token
               }
             })
             .then(res => {
-              apiData = res.data.items.filter(episode =>
+              const apiData = res.data.items.filter(episode =>
                 [episodeNum + 1, episodeNum, episodeNum - 1].includes(getEpNum(episode.name))
               );
 
               const name = apiData[apiData.length - 2]?.name
                 .toLowerCase()
-                .replace(/[’':,.-]/g, '')
+                .replace(/[’':,.]/g, '')
+                .replace(' - ', '-')
                 .replace(/\s+/g, '-');
 
               axios
-                .get(pcURL + name)
+                .get(config.castURL + name)
                 .then(res => {
-                  webData = res.data;
-                  if (apiData.length && webData) {
-                    const data = {
-                      webData,
-                      previous: apiData.pop(),
-                      episode: apiData.pop(),
-                      next: apiData.pop()
-                    };
+                  const data = {
+                    webData: res.data,
+                    previous: apiData.pop(),
+                    episode: apiData.pop(),
+                    next: apiData.pop()
+                  };
 
-                    let result = template.toString();
+                  let result = template.toString();
 
-                    Object.keys(tagHandlers).forEach(tag => {
-                      result = result.replace(tag, tagHandlers[tag](data));
-                    });
+                  Object.keys(tagHandlers).forEach(tag => {
+                    result = result.replace(tag, tagHandlers[tag](data));
+                  });
 
-                    console.log(result);
-                  }
+                  console.log(result);
                 })
                 .catch(err => {
-                  throw new Error(`${pcURL + name} not found: ${err}`);
+                  throw new Error(`${config.castURL + name} not found: ${err}`);
                 });
             });
         })
